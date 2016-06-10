88726fd19c84538d9955f9acda816a9c6d037021
Richard Hsu <rhsu@gwu.edu>
2016-06-10 16:16:45 -0400

    Fixing typo

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require './films/films'
 require './response_object/response_object'
 require './planets/planets'
 require './starships/starships'
-require 'alexa_verifer'
+require 'alexa_verifier'
 #use Rack::Env, envfile: 'config/local_env.yml'
 
 post '/' do
