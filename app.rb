24a42dd8015c498bcbb607b94fcd2b1d1589f684
Richard Hsu <rhsu@gwu.edu>
2016-06-10 15:19:35 -0400

    Trying to look at header

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ post '/' do
   request.body.rewind
 
   puts "---REQUEST PAYLOAD---"
-  puts request.headers
+  puts request.header
   @request_payload = JSON.parse request.body.read
 
   puts "---REQUEST PAYLOAD---"
